a6a36c41577a4fe0883c232d473ca947874cbc06
Milos Veljkovic Velja <milos.veljkovic@htecgroup.com>
2022-10-17 15:00:48 +0200

    extract workplace to separate page

---
+++ src/store/workplace.ts
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+import { ChannelType } from 'components/sidebar/sidebar';
+
+export type WorkplaceType = {
+  id: string;
+  name: string;
+  channels: ChannelType[];
+};
+
+const initialState: WorkplaceType = {
+  id: '',
+  name: '',
+  channels: [],
+};
+
+const workplaceSlice = createSlice({
+  name: 'workplace',
+  initialState,
+  reducers: {
+    setWorkplace(state, action: PayloadAction<WorkplaceType>) {
+      state.id = action.payload.id;
+      state.name = action.payload.name;
+      state.channels = action.payload.channels;
+    },
+    resetWorkplace(state) {
+      state.id = initialState.id;
+      state.name = initialState.name;
+      state.channels = initialState.channels;
+    },
+  },
+});
+
+export const { setWorkplace, resetWorkplace } = workplaceSlice.actions;
+
+export default workplaceSlice;
