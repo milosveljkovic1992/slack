c1e361b646c30be9dfebe16257b2dd469b6a58f9
Milos Veljkovic Velja <milos.veljkovic@htecgroup.com>
2022-10-20 22:18:06 +0200

    create landing page route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,9 @@
 import { App } from 'App';
 import { Channel } from 'components';
 import { Workplace } from 'pages/workplace';
+import { LandingPage } from 'pages/landing-page';
 import { createBrowserRouter } from 'react-router-dom';
+import { SignUpModal } from 'components/sign-up-modal/sign-up-modal';
 
 export const router = createBrowserRouter([
   {
@@ -9,6 +11,16 @@ export const router = createBrowserRouter([
     element: <App />,
     errorElement: <h1>Not found</h1>,
     children: [
+      {
+        path: '/',
+        element: <LandingPage />,
+        children: [
+          {
+            path: '/sign-up',
+            element: <SignUpModal />,
+          },
+        ],
+      },
       {
         path: '/:workplaceId',
         element: <Workplace />,
