b383b3fa0c8f71de3d96ac01a9b701b128d19952
Milos Veljkovic Velja <milos.veljkovic@htecgroup.com>
2022-10-27 10:04:49 +0200

    create loading component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { Channel } from 'components/channel/channel';
 export { Header } from 'components/header/header';
+export { LoadingSpinner } from 'components/loading-spinner/loading-spinner';
 export { LoginModal } from 'components/login-modal/login-modal';
 export { Message } from 'components/message/message';
 export { Sidebar } from 'components/sidebar/sidebar';
